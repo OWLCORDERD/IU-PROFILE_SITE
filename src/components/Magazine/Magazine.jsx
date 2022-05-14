01bdf502aa384b836535627683ee4acf55d8fbc4
MIYEOK <lim5570@naver.com>
2022-05-15 00:10:14 +0900

    05/15 commit

--- src/components/Magazine/Magazine.jsx
+++ src/components/Magazine/Magazine.jsx
@@ -1,10 +1,26 @@
 import React, {useState} from 'react'
 import './magazine.css'
 import Data from "../ImageData.json"
+import Popup from "../Popup/Popup"
+import { AnimatePresence, AnimateSharedLayout } from 'framer-motion'
 
 const Magazine = () => {
 
   const [filter, onFilter] = useState("#vogue");
+
+  const [popup, setPopup] = useState(false);
+
+  const [layoutId,setlayoutId] = useState(null);
+
+  const poper = (id) =>{
+    if(popup===false){
+      setlayoutId(id);
+      setPopup(!popup);
+    }else{
+      setPopup(!popup);
+      setlayoutId(null);
+    }
+  }
 
   return (
     <div className = "magazine-container">
@@ -20,21 +36,27 @@ const Magazine = () => {
           <a href = "#MARIE CLAIRE" onClick={()=>onFilter("#MARIE CLAIRE")} className = {filter === "#MARIE CLAIRE" ? "active" : ''}>MARIE CLAIRE</a>
         </div>
 
+        <AnimateSharedLayout>
         <div id = "magazine-imageBox" className={filter === "#vogue" ? "active" : ''}>
 
           {Data.VogueData.map((item) => {
             return(
           <div className = "magazine-item">
-          <img src = {item.img_url} alt = ""/>
+          <img src = {item.img_url} alt = "" layoutId = {item.id}/>
           <div className = "magazine-detail">
             <img src = {item.title} alt = ""/>
-            <p>{item.sub_title}</p>
+            <input type = "submit" value = "VIEW MORE" className= "View_button"  onClick={()=> poper(item.id)}/>
           </div>
           </div>
             )
           })}
-
+        
           </div>
+          <AnimatePresence>
+            {popup&&<Popup poper = {poper} layoutId = {layoutId}/>}
+            </AnimatePresence>
+
+          </AnimateSharedLayout>
 
           
         <div id = "magazine-imageBox" className={filter === "#MARIE CLAIRE" ? "active" : ''}>
