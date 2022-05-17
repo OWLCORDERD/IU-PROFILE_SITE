c0d6661dbb49742b7edc234b5a1b1a39dea8e16f
MIYEOK <lim5570@naver.com>
2022-05-17 23:39:31 +0900

    5/17 commit

--- src/components/Gallary/Gallary.jsx
+++ src/components/Gallary/Gallary.jsx
@@ -1,67 +1,48 @@
 import React from 'react'
 import './gallary.css'
-import GallaryPicture1 from "../../assets/image/J.ASTINA/2022 JASTINA_1.jpg"
-import GallaryPicture2 from "../../assets/image/J.ASTINA/2022 JASTINA_2.jpg"
-import GallaryPicture3 from "../../assets/image/J.ASTINA/2022 JASTINA_3.jpg"
-import GallaryPicture4 from "../../assets/image/J.ASTINA/2022 JASTINA_4.jpg"
-import GallaryPicture5 from "../../assets/image/J.ASTINA/2022 JASTINA_5.jpg"
-import GallaryPicture6 from "../../assets/image/J.ASTINA/2022 JASTINA_6.jpg"
-import GallaryPicture7 from "../../assets/image/J.ASTINA/2022 JASTINA_7.jpg"
-import GallaryPicture8 from "../../assets/image/J.ASTINA/2022 JASTINA_8.jpg"
+import data from "../ImageData.json"
+import background from "../../assets/image/J.ASTINA/HIP SUMMER/HIP SUMMER COLLECTION-main.jpg"
 
 const Gallary = () => {
   return (
     <div className = "Gallary-container">
 
-      <div className = "Gallary-title">
-        <h1>2022 NEW IU COLLECTION</h1>
-      </div>
-
     <div className = "Gallary-box">
       <div className = "Gallary-item">
 
+      <div className = "Gallary-mainbox">
+
+        <img src = {background} alt = ""/>
+
+      <div className = "Gallary-title">
+        <h1>2022 J.ESTINA X IU COLLECTION</h1>
+      </div>
+
+
         <div className = "Gallary-video">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/dGqj-lcTMlY"
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/eUI0pqOYeic"
            title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
             allowfullscreen></iframe>
         </div>
 
+        <div className = "Gallary-sub-title">
+        <h1>HIP SUMMER</h1>
+        <p>22 SUMMER COLLECTION</p>
+        <button className = "JESTINA">VIEW MORE</button>
+      </div>
+
+      </div>
       </div>
 
       <div className = "Gallary-item">
         <div className = "Gallary-list">
+          {data['J.ESTINA'].map((item)=> {
+            return(
           <div className = "Gallary-sub-img">
-            <img src = {GallaryPicture1} alt = ""/>
+           <img src = {item.img_url} alt = ""/>
           </div>
-
-          <div className = "Gallary-sub-img">
-            <img src = {GallaryPicture2} alt = ""/>
-          </div>
-
-          <div className = "Gallary-sub-img">
-            <img src = {GallaryPicture3} alt = ""/>
-          </div>
-
-          <div className = "Gallary-sub-img">
-            <img src = {GallaryPicture4} alt = ""/>
-          </div>
-
-          <div className = "Gallary-sub-img">
-            <img src = {GallaryPicture5} alt = ""/>
-          </div>
-
-          <div className = "Gallary-sub-img">
-            <img src = {GallaryPicture6} alt = ""/>
-          </div>
-
-          <div className = "Gallary-sub-img">
-            <img src = {GallaryPicture7} alt = ""/>
-          </div>
-          
-          <div className = "Gallary-sub-img">
-            <img src = {GallaryPicture8} alt = ""/>
-          </div>
-
+          )
+          })}
           </div>
       </div>
 
