8d00f8d11ddeb0a6277b2708870d94f4585bf1d0
MIYEOK <lim5570@naver.com>
2022-05-26 23:07:40 +0900

    05/26 commit

--- src/components/Youtube/Youtube.jsx
+++ src/components/Youtube/Youtube.jsx
@@ -1,21 +1,25 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import "./youtube.css";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import data from "../ImageData.json";
+import {AiFillPlayCircle} from "react-icons/ai"
 
 
-export default class SimpleSlider extends Component {
-  render() {
+const Youtube = () => {
+    
     const settings = {
       infinite: true,
       slidesToShow: 3,
       slidesToScroll: 1,
       autoplay: true,
-      speed: 2000,
-      autoplaySpeed: 6000,
+      speed: 1000,
+      autoplaySpeed: 5000,
     };
+
+    const [categoryClick, setCategoryClick] = useState(null);
+    
 
     return (
 
@@ -26,27 +30,59 @@ export default class SimpleSlider extends Component {
     </div>
 
     <ul className = "Youtube-category">
-      <li><a href = "#" className = "active">IU TV</a></li>
-      <li><a href = "#">IU CLIP</a></li>
+      <li><a href = "#IU_TV" onClick={() => setCategoryClick('IU_TV')} className = {categoryClick === 'IU_TV' ? 'active' : ''}>IU TV</a></li>
+      <li><a href = "#IU_CLIP" onClick={() => setCategoryClick('IU_CLIP')} className = {categoryClick === 'IU_CLIP' ? 'active' : ''}>IU CLIP</a></li>
       <li><a href = "#">아이유의 팔레트</a></li>
       <li><a href = "#">꿈빛 셰프유</a></li>
       <li><a href = "#">BEHIND</a></li>
     </ul>
     
 
-      <div className = "slide-container">
+      <div id = "IU_TV_slide-container" className = {categoryClick === 'IU_TV' ? 'active' : ''}>
 
         <Slider {...settings}>
-          {data.VogueData.map((item, index) => {
+          {data.IU_TV.map((item, index) => {
             return (
           <div className = "slide-box" key = {index}>
-           <img src = {item.img_url} alt = ""/>
+            <div className = "play-video">
+              <AiFillPlayCircle className = "play-button"/>
+            </div>
+           <img src = {item.img_url} alt = "" key={item.id}/>
+           <div className = "slide-title">
+             <h1 key={item.id}>{item.title}</h1>
+             <p key={item.id}>{item.director}</p>
+           </div>
           </div>
         )
           })}
         </Slider>
+        
       </div>
+
+      <div id = "IU_CLIP_slide-container" className = {categoryClick === 'IU_CLIP' ? 'active' : ''}>
+        
+        <Slider {...settings}>
+          {data.IU_CLIP.map((item, index) => {
+            return (
+          <div className = "slide-box" key = {index}>
+            <div className = "play-video">
+              <AiFillPlayCircle className = "play-button"/>
+            </div>
+           <img src = {item.img_url} alt = "" key={item.id}/>
+           <div className = "slide-title">
+             <h1 key={item.id}>{item.title}</h1>
+             <p key={item.id}>{item.director}</p>
+           </div>
+          </div>
+        )
+          })}
+        </Slider>
+        
+      </div>
+
+
       </div>
     );
   }
-}
+
+export default Youtube;
